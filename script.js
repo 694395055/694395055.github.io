940e57b821db9d3eaa589f881f7df03d4152e2bf
zhou <694395055@qq.com>
2024-06-14 19:38:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const greetingMessage = document.getElementById('greetingMessage');
 // 为按钮添加点击事件监听器  
 greetButton.addEventListener('click', function() {  
     // 当按钮被点击时，显示问候消息  
-    greetingMessage.textContent = '亲爱的爸爸，祝您父亲节快乐！';  
+    greetingMessage.textContent = '亲爱的老爸，祝您父亲节快乐！';  
 });
